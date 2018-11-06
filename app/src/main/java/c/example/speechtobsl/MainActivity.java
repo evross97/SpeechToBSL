20491d9b7739e0512886e5f147f2b80d240bbb62
Ella <evross97@gmail.com>
2018-11-06 18:19:35 +0000

    EOD 6.11 - Extracted into class but not displaying back on screen.

--- app/src/main/java/c/example/speechtobsl/MainActivity.java
+++ app/src/main/java/c/example/speechtobsl/MainActivity.java
@@ -1,12 +1,8 @@
 package c.example.speechtobsl;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.speech.RecognitionListener;
-import android.speech.RecognizerIntent;
-import android.speech.SpeechRecognizer;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -18,17 +14,17 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements RecognitionListener{
+public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
     private final String LOG_TAG = "BSL App";
 
     private Button mRecordButton = null;
     private TextView mRecordText = null;
+    private TextView mTextConverted = null;
     private boolean mStartRecording = false;
 
-    private SpeechRecognizer speech = null;
-    private Intent recogniserIntent;
+    private SpeechRecognitionListener speech = null;
 
     private boolean permissionToRecordAccepted = false;
     private String[] permissions = {Manifest.permission.RECORD_AUDIO};
@@ -43,6 +39,8 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
         ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
         mRecordButton = findViewById(R.id.recordButton);
         mRecordText = findViewById(R.id.record_text);
+        mTextConverted = findViewById(R.id.text_converted);
+
         mRecordText.setText("Press the red button to start recording");
         mRecordButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,63 +53,8 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
             }
         });
 
-        speech = SpeechRecognizer.createSpeechRecognizer(this);
-        speech.setRecognitionListener(this);
-        recogniserIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        recogniserIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "en");
-        recogniserIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-
-    }
-
-    @Override
-    public void onBeginningOfSpeech() {
-        Log.i(LOG_TAG, "onBeginningOfSpeech");
-    }
-
-    @Override
-    public void onRmsChanged(float v) {
-        Log.i(LOG_TAG, "onRmsChanged");
-
-    }
-
-    @Override
-    public void onBufferReceived(byte[] bytes) {
-        Log.i(LOG_TAG, "onBufferReceived: " + bytes);
-
-    }
-
-    @Override
-    public void onEndOfSpeech() {
-        Log.i(LOG_TAG, "onEndOfSpeech");
-    }
-
-    @Override
-    public void onError(int code) {
-        Log.i(LOG_TAG, "FAILED with error code " + String.valueOf(code));
-
-    }
-
-    @Override
-    public void onReadyForSpeech(Bundle arg) {
-        Log.i(LOG_TAG, "onReadyForSpeech");
-    }
-
-    @Override
-    public void onPartialResults(Bundle arg) {
-        Log.i(LOG_TAG, "onPartialResults");
-    }
-
-    @Override
-    public void onEvent(int i, Bundle bundle) {
-        Log.i(LOG_TAG, "onEvent");
-
-    }
-
-    @Override
-    public void onResults(Bundle data) {
-        ArrayList<String> matches = data.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-        String returnedText = matches.get(0);
-        mRecordText.setText(returnedText);
+        speech = new SpeechRecognitionListener(this);
+        mTextConverted.setText(speech.decodedSpeech);
     }
 
     @Override
@@ -130,10 +73,11 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
     public void onRecord(boolean start) {
         if(start) {
             mRecordText.setText("Press the button again to stop recording");
-            speech.startListening(recogniserIntent);
+            speech.startListening();
         } else {
             mRecordText.setText("Press the red button to start recording");
             speech.stopListening();
+            mTextConverted.setText(speech.decodedSpeech);
         }
     }
 
