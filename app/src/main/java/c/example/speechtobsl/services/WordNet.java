c770850fd1711ca84e64b128704c3608a847ec29
Ella <evross97@gmail.com>
2019-02-04 14:48:21 +0000

    RiWordnet - not working

--- app/src/main/java/c/example/speechtobsl/services/WordNet.java
+++ app/src/main/java/c/example/speechtobsl/services/WordNet.java
@@ -1,33 +1,17 @@
 package c.example.speechtobsl.services;
 
 import android.content.Context;
-import android.content.res.AssetManager;
-import android.net.Uri;
-import android.os.Environment;
-import android.util.Log;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Arrays;
 
-import edu.mit.jwi.Dictionary;
-import edu.mit.jwi.IDictionary;
-import edu.mit.jwi.item.IIndexWord;
-import edu.mit.jwi.item.ISynset;
-import edu.mit.jwi.item.IWord;
-import edu.mit.jwi.item.IWordID;
+import rita.RiString;
+import rita.RiWordNet;
+
 
 public class WordNet {
 
-    private IDictionary dict;
+    private RiWordNet wordnet;
     private final String LOG_TAG = "WordNet";
     private Context appCtx;
 
@@ -35,31 +19,14 @@ public class WordNet {
         this.appCtx = ctx;
         StringBuffer fpath = new StringBuffer(400);
         fpath.append(appCtx.getExternalFilesDir(null).toString()
-                + "/dict");
-        File f = new File(fpath.toString());
-        dict = new Dictionary(f);
-        try {
-            dict.open();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+                + "/WordNet-3.0");
+        wordnet = new RiWordNet(fpath.toString());
     }
 
     public ArrayList<String> getSynonyms(String word) {
         ArrayList<String> syns = new ArrayList<>();
         //Converter converter = new Converter();
-        try {
-            IIndexWord idxWord = dict.getIndexWord(word.toLowerCase(), edu.mit.jwi.item.POS.NOUN);
-            IWordID iWordID = idxWord.getWordIDs().get(0);
-            IWord wordMeaning = dict.getWord(iWordID);
-            ISynset synsWords = wordMeaning.getSynset();
-            for(IWord synonym: synsWords.getWords()) {
-                syns.add(synonym.getLemma());
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-            System.out.println("Unable to open dictionary");
-        }
+        syns.addAll(Arrays.asList(wordnet.getSynonyms(word, RiWordNet.NOUN, 10)));
         return syns;
     }
 }
