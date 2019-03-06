4a4164a4bb6ee9598421e092b195d2ae66ed65de
Ella <evross97@gmail.com>
2019-03-06 14:03:22 +0000

    More changes.

--- app/src/main/java/c/example/speechtobsl/outer_framework/StartUpActivity.java
+++ app/src/main/java/c/example/speechtobsl/outer_framework/StartUpActivity.java
@@ -1,7 +1,12 @@
 package c.example.speechtobsl.outer_framework;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import c.example.speechtobsl.R;
 
 public class StartUpActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class StartUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_up);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void startMain(View view) {
+        Intent main = new Intent(this, SpeechInputActivity.class);
+        startActivity(main);
     }
 }
